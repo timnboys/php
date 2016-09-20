903f2477e7149caad3c7d021987f23b34ce5aa70
Ben Magee <bmagee@cogative.com>
2016-09-20 18:20:20 +0100

    Allow API organisation and API key to be set after initial instantiation of the object.
    The Product class now retrieves the product's unique properties if
    the API object passed to the Product object has an API key attached. Obviously, the API key must be valid and able to access the product specified.
    
    Added getter methods for all the new properties that are now fetched.
    
    Added an update() method for future implementation. This method will allow you to update a resource directly from this library. May not be supported for all API resource types (eg: products) .
    
    Wrote tests to support new product functions.

--- src/API/Api.php
+++ src/API/Api.php
@@ -14,6 +14,9 @@ require_once dirname(__FILE__) . '/../../vendor/autoload.php';
 class Api
 {
     protected $client;
+    protected $organisation;
+
+    private $key;
 
     /**
      * @var ResponseInterface $lastResponse
@@ -31,10 +34,21 @@ class Api
 
     public function __construct($organisation, $key)
     {
+        // Set the API key
+        $this->key = $key;
+
+        // Set the organisation
+        $this->organisation = $organisation;
+
+        // Setup the HTTP client
+        $this->initialiseClient();
+    }
+
+    protected function initialiseClient() {
         $this->client = new Client([
-            'base_uri' => 'https://'.$organisation.'.enverido.com',
+            'base_uri' => 'https://'.$this->organisation.'.enverido.com',
             'headers' => [
-                'X-API-KEY' => $key
+                'X-API-KEY' => $this->key
             ]
         ]);
     }
@@ -75,5 +89,31 @@ class Api
 
     public function getClient() {
         return $this->client;
+    }
+
+    /**
+     * Return whether or not we're passing an API key with these requests.
+     * @return boolean Whether or not API key is present
+     */
+
+    public function authenticated() {
+        return $this->key != null;
+    }
+
+    /**
+     * Set the API key to pass with requests. This method can be used to change the enverido account
+     * you're authenticating as, or to add an API key to this instance so that protected API endpoints can
+     * be called.
+     *
+     * @param string $organisation Organisation to query (find it in your enverido account)
+     * @param string $key API key
+     */
+
+    public function authenticate($organisation, $key) {
+       $this->organisation = $organisation;
+        $this->key = $key;
+
+        // reinitialise client using the new values
+        $this->initialiseClient();
     }
 }
--- src/API/Licence.php
+++ src/API/Licence.php
@@ -101,4 +101,18 @@ class Licence implements ApiResource
     public function setId($id) {
         $this->id = $id;
     }
+
+    /**
+     * Update the resource at the enverido licence server. This requires a valid API
+     * key to have been set. The properties set in the instance of the object are used
+     * as the values to send to the licence server.
+     *
+     * @return boolean Returns true if update was successful
+     */
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+        return false;
+    }
 }
